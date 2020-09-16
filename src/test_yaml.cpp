076f707dd309a679ed40f31a18bd7308144035bb
Quynh Nguyen Khanh <nkquynh1998@gmail.com>
2020-09-16 10:14:29 +0200

    add parameter yaml

---
+++ src/test_yaml.cpp
@@ -0,0 +1,25 @@
+#include "ros/ros.h"
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "talker");
+  ros::NodeHandle nh;
+  double number_to_get;
+  std::vector<std::string> my_list;
+  ros::Rate loop_rate(10);
+  while(ros::ok())
+  {
+      nh.getParam("/number_float", number_to_get);
+      nh.getParam("/dictionary",my_list);
+      for (unsigned i=0; i<my_list.size();i++)
+      {
+          ROS_INFO_STREAM(my_list[i]);
+      }
+      //ROS_INFO_STREAM(number_to_get);
+      ros::spinOnce();
+      loop_rate.sleep();
+  }
+
+  return 0;
+  
+}
